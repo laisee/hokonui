c1e40da5ccc4eac96b7ee19e357fdaba8b61d57a
G Costin <tinwald@gmail.com>
2015-07-13 17:55:40 +0800

    moved okcoin to /ignore until 404 issue resolved

--- test/testokcoin.py
+++ test/testokcoin.py
@@ -1,6 +1,6 @@
 import json
 from exchanges.okcoin import OKCoin as okc 
 
+print 'Price ', okc.get_current_price()
 print 'Ask   ', okc.get_current_ask()
 print 'Bid   ', okc.get_current_bid()
-print 'Price ', okc.get_current_price()
