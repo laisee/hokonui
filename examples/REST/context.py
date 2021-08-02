d17fac24631fdeac9ad55fec17116c23f4e47ffd
laisee <laisee@users.noreply.github.com>
2021-08-02 23:39:36 +0800

    Update context.py

--- examples/REST/context.py
+++ examples/REST/context.py
@@ -4,7 +4,6 @@ if __package__ is None:
     import sys
     from os import path
     sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
-
 import hokonui
 import hokonui.exchanges
 import hokonui.utils
